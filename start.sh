d65d6977589edc8ba97c9b10e37518e961b15757
Moviestorebot <106879858+Santosh344p@users.noreply.github.com>
2023-11-24 20:29:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Santosh344p/Moviesfgfu-Bot.git /Moviesfgfu-Bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Moviesfgfu-Bot 
 fi
-cd /VJ-FILTER-BOT 
+cd /Moviesfgfu-Bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
